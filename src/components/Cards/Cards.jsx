cb15846d0af0d4ccc2f170847bde5d644d0929d7
Agustin E. Di Giambatista <agustindigiambatista@gmail.com>
2023-09-04 11:35:35 -0300

    Cards

---
+++ src/components/Cards/Cards.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import './Cards.css'
+
+function Cards() {
+  return (
+    <>
+    <section className='Card_content' action="">
+      <article className='Card'>
+        <p>Enjoy our parks on special discount!</p>
+        <button>See more!</button>
+      </article>
+      <article className='Card'>
+        <p>Enjoy our parks on special discount!</p>
+        <button>See more!</button>
+      </article>
+      <article className='Card'>
+        <p>Enjoy our parks on special discount!</p>
+        <button>See more!</button>
+      </article>
+      <article className='Card'>
+        <p>Enjoy our parks on special discount!</p>
+        <button>See more!</button>
+      </article>
+    </section>
+    </>
+  )
+}
+
+export default Cards
---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import './Main.css'
+
+function Main() {
+  return (
+    
+    <header className='Menu_Content'>
+        <h1>FairyWorld</h1>
+        <nav>
+            <ul>
+                <li><a href="">Parks</a></li>
+                <li><a href="">Tickets</a></li>
+                <li><a href="https://www.github.com/Agusdigiam">About us</a></li>
+            </ul>
+        </nav>
+    </header>
+    
+  )
+}
+
+export default Main
